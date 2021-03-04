59de8a983502c5a4353665c55cf02072963dc073
Lucas Hanke <lucas.hanke@sanar.com>
2021-03-04 18:53:41 -0300

    create custom metric for invalid transactions count

--- src/billing/services/transactions.service.ts
+++ src/billing/services/transactions.service.ts
@@ -1,3 +1,4 @@
+import { CounterMetric, PromService } from '@digikare/nestjs-prom';
 import { Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from '../controllers/dtos/transaction.dto';
 import Transaction from '../domain/transaction.domain';
@@ -5,17 +6,28 @@ import { TransactionsRepository } from '../persistance/transaction.repository';
 
 @Injectable()
 export class TransactionsService {
+  private readonly invalidCount: CounterMetric;
+
   constructor(
     private readonly transactionsRepository: TransactionsRepository,
-  ) {}
+    private readonly promService: PromService,
+  ) {
+    this.invalidCount = this.promService.getCounter({
+      name: 'invalid_transaction_count',
+    });
+  }
 
   async create(
     createTransactionDto: CreateTransactionDto,
   ): Promise<Transaction> {
     const { amount, paymentMethod, userId } = createTransactionDto;
-    const transaction = Transaction.create(amount, paymentMethod, userId);
-
-    return this.transactionsRepository.save(transaction);
+    try {
+      const transaction = Transaction.create(amount, paymentMethod, userId);
+      return this.transactionsRepository.save(transaction);
+    } catch (error) {
+      this.invalidCount.inc();
+      throw error;
+    }
   }
 
   async findAll(): Promise<Transaction[]> {
